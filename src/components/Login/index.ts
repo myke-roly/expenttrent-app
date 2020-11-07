5d7e0fb1d50f6a643bb9a1390998d03ec5ef17b8
mike <myke_roly@hotmail.com>
2020-11-07 00:05:05 -0300

    hidden login if is succesful

--- src/components/Login/index.ts
+++ src/components/Login/index.ts
@@ -5,6 +5,8 @@ interface DataLoginI {
   password: string;
 }
 
+const login = document.querySelector('.login');
+
 function getValuesInputs(): DataLoginI {
   const email = document.querySelector('#email') as HTMLInputElement,
     password = document.querySelector('#password') as HTMLInputElement;
@@ -19,4 +21,7 @@ export function showFormLogin(e: Event): void {
   e.preventDefault();
   const { email, password } = getValuesInputs();
   firebase.singIn(email, password);
+
+  // TODO: validar que no hay error;
+  login.classList.add('hidden-elem');
 }
