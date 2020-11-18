3ac145f378d8914907afe85fdbeedc80fba66a26
mike <myke_roly@hotmail.com>
2020-11-18 01:56:47 -0300

    bit refactor

--- src/components/Gasto/modalAddIngreso.ts
+++ src/components/Gasto/modalAddIngreso.ts
@@ -2,25 +2,25 @@ import { hiddenElement, showElement } from '../../helpers/toggleElement';
 
 const modal = document.querySelector('.modal__addIngreso') as HTMLElement;
 const displayIngresos = document.querySelector('#ingresos') as HTMLElement;
+const btnAdd = document.querySelector('.modal__addIngreso--btn');
+const newIngreso = document.querySelector('#ingreso') as HTMLInputElement;
+const btn__closeModal = document.querySelector('.close__addIngreso');
 
 export function openModal(): void {
   showElement(modal);
-
-  const btnAdd = document.querySelector('.modal__addIngreso--btn');
-  const newIngreso = document.querySelector('#ingreso') as HTMLInputElement;
-
   btnAdd.addEventListener('click', () => addIngreso(Number(newIngreso.value)));
-  const btn__closeModal = document.querySelector('.close__addIngreso');
 
-  btn__closeModal.addEventListener('click', () => {
-    hiddenElement(modal);
-    newIngreso.value = '';
-  });
+  btn__closeModal.addEventListener('click', hiddenModal);
 }
 
+function hiddenModal() {
+  hiddenElement(modal);
+  newIngreso.value = '';
+}
+
+let ingresos: number = 0;
 function addIngreso(newIngreso: number): void {
   // get ingresos previos
-  let ingresos: number = 0;
 
   // sumarle el nuevo valor ingresado
   if (newIngreso > 0) {
@@ -28,6 +28,8 @@ function addIngreso(newIngreso: number): void {
     // guardar el nuevo valor en DB
 
     displayIngresos.textContent = ingresos.toString() + ' $';
+    hiddenModal();
+
     return;
   }
 
